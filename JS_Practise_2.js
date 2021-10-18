83414e231833acb0e92cb93938234ae516128ee2
amar360 <amar.tariq1993@gmail.com>
2021-10-18 10:17:10 -0500

    reArrange Files

---
+++ JS_Practise_2.js
@@ -0,0 +1,307 @@
+// Array Spilce
+// sum of array = 10
+let array = [2, 4, 5, 1, 7, 5, 2, 1];
+let newArray = array.splice(2, 2);
+console.log(array, newArray);
+
+// array splice -- Add
+let num = [10, 11, 12, 12, 15];
+let newNum = num.splice(3, 1, 'Amar', 'Tariq');
+console.log(num, newNum);
+
+// array slice
+let array2 = [10, 11, 12, 13, 14];
+let arraySlice = array2.slice(2, 5); // start - end
+console.log(array2, arraySlice);
+
+// indexOf returns index if found
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+console.log(fruits.indexOf('dates'));
+console.log(fruits.indexOf('oranges'));
+console.log(fruits.indexOf('pears'));
+
+// deepest array
+let nestedArray = [['deep'], [['deeper'], ['deeper']], [[['deepest'], ['deepest']], [[['deepest-est?']]]]];
+console.log(nestedArray[2][1][0][0][0]);
+
+// factorial
+function factorialize(num) {
+  return num < 2 ? 1 : num * factorialize(num - 1);
+}
+console.log(factorialize(3));
+
+// ends with 'sub string'
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target;
+}
+console.log(confirmEnding('Bastian', 'an'));
+
+// repeat string NUM times
+function repeatStringNumTimes(str, num) {
+  return num === 0 ? '' : str + repeatStringNumTimes(str, num - 1);
+}
+console.log(repeatStringNumTimes('*', 3));
+console.log(repeatStringNumTimes('abc', 0));
+
+// 'truth Test' returns ture if test is passed else false
+function findElement(arr, fn) {
+  return arr.some((num) => {
+    if (fn(num)) {
+      return num;
+    }
+  });
+}
+console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
+
+// Title Case a Sentence
+function titleCase(str) {
+  let newStr = str.split(' ').map((val) => {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return newStr.join(' ');
+}
+console.log(titleCase("I'm a little tea pot"));
+
+// Prototype Example
+Array.prototype.myUcase = function () {
+  const UArray = [];
+  for (let fruit of this) {
+    UArray.push(fruit.toUpperCase());
+  }
+  return UArray;
+};
+
+const myFruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+console.log(myFruits.myUcase());
+
+// Prototype MAP function
+Array.prototype.myMap = function (callback) {
+  const newArray = [];
+  for (let num of this) {
+    newArray.push(callback(num));
+  }
+  return newArray;
+};
+const arr = [1, 2, 3].myMap((num, i, myNum) => num * 2);
+console.log(arr);
+
+// Copy each element of  the first array into the second array, in order.
+function frankenSplice(arr1, arr2, n) {
+  let newArr = [...arr2];
+  newArr.splice(n, 0, ...arr1);
+  return newArr;
+}
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+// Remove all falsy values from an array.
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+console.log(bouncer([7, 'ate', '', false, 9]));
+
+// get the Index
+
+function getIndexToIns(arr, num) {
+  let newArr = [...arr];
+  let getIndex = newArr
+    .sort((a, b) => a - b)
+    .indexOf((element, index) => {
+      if (element > num) {
+        return index;
+      }
+    });
+  return getIndex;
+}
+console.log(getIndexToIns([60, 40, 20, 70], 50));
+
+// split array in 2D
+
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2));
+
+// concat adds easily
+
+let arr3 = [1, 2, 3];
+console.log(arr3.concat([1, 2, 4]));
+
+// sort alpha
+
+function reverseAlpha(arr) {
+  return arr.sort((a, b) => {
+    return a > b ? 1 : -1;
+  });
+}
+console.log(reverseAlpha(['c', 'b', 'a', 'd', 'e']));
+
+// Currying function
+
+function curried(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+let cur = curried(1);
+console.log(cur(3));
+console.log(curried(1)(2));
+
+// Sum All Numbers in a Range
+
+function sumAll(arr) {
+  let sum = 0;
+  let newArr = [...arr].sort((a, b) => a - b);
+  for (let i = newArr[0]; i <= newArr[1]; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumAll([10, 5]));
+
+// difference of 2 arrays
+
+function diffArray(arr1, arr2) {
+  function check(a, b) {
+    return a.filter((item) => b.indexOf(item) === -1);
+  }
+  return [...check(arr1, arr2), ...check(arr2, arr1)];
+}
+
+console.log(diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]));
+
+// remove arguments
+function destroyer(arr, ...args) {
+  console.log(Object.values(arguments).slice(1));
+  return arr.filter((item) => args.indexOf(item) === -1);
+}
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+// replace string
+
+function myReplace(str, before, after) {
+  return str.replace(before, after);
+}
+
+console.log(myReplace('A quick brown fox jumped over the lazy dog', 'jumped', 'leaped'));
+
+// DNA paring AT TA GC CG
+
+function pairElement(str) {
+  let pairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+  };
+  var arr = str.split('');
+  return arr.map((x) => [x, pairs[x]]);
+}
+console.log(pairElement('GCAT'));
+
+// Find the missing letter in the passed letter range and return it.
+
+function fearNotLetter(str) {
+  console.log('a'.charCodeAt());
+  console.log(String.fromCharCode(97));
+}
+console.log(fearNotLetter('abcdefghjklmno'));
+
+// Union of unique Array
+
+function uniteUnique(...arr) {
+  return [...new Set([].concat(...arr))];
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+// calculate tips and total
+
+billCalculator = (billArray) => {
+  const tipArray = [];
+  const totalBill = [];
+  let tip = 0;
+  billArray.forEach((bill) => {
+    if (bill <= 50) {
+      tip = bill * 0.2;
+      tipArray.push(tip);
+    }
+    if (bill > 50 && bill <= 100) {
+      tip = bill * 0.15;
+      tipArray.push(tip);
+    }
+    if (bill > 100) {
+      tip = bill * 0.1;
+      tipArray.push(tip);
+    }
+  });
+  for (let i = 0; i < billArray.length; i++) {
+    totalBill[i] = tipArray[i] + billArray[i];
+  }
+  console.log(tipArray);
+  console.log(totalBill);
+};
+
+const bills = [25, 55, 255];
+
+billCalculator(bills);
+
+// Interface
+
+var car = new Vehicle('Honda', 'white', '2010', 'UK');
+console.log(car, car.model);
+
+function Vehicle(model, color, year, country) {
+  this.model = model;
+  this.color = color;
+  this.year = year;
+  this.country = country;
+}
+
+// TALLY
+
+const fruitBasket = [
+  'banana',
+  'cherry',
+  'orange',
+  'apple',
+  'cherry',
+  'orange',
+  'apple',
+  'banana',
+  'cherry',
+  'orange',
+  'fig',
+];
+
+const count1 = fruitBasket.reduce((tally, fruit) => {
+  tally[fruit] = (tally[fruit] || 0) + 1;
+  return tally;
+}, {});
+
+console.log(count1);
+
+// Tally with REDUCE
+
+const fruitBasket2 = ['banana', 'banana', 'cherry'];
+
+const count2 = fruitBasket2.reduce((tally, fruit) => {
+  console.log(tally, fruit, tally[fruit]);
+  if (!tally[fruit]) {
+    tally[fruit] = 1;
+  } else {
+    tally[fruit] = tally[fruit] + 1;
+  }
+  return tally;
+}, {});
+
+console.log(count2);
---
+++ JavaScriptES6.js
@@ -0,0 +1,346 @@
+/* 
+ 1) Convert Object into Array and Print out its Values
+ 2) Reserve String
+ 3) clone an Object and print Clone_Object.a.b.c
+ 4) Merge 2 Array and sort in Descending Order
+ 5) add elements in an array
+ 6) Double the even number in an array
+ 7) filter out cities with more than 50M population.
+ 8) Select only the dogs
+    Translate their ages into dog years (multiply them by seven)
+    Sum the results
+ 9) Make this work adding(2)(3)
+ 10) Math.min , Math.max
+ 11) find missing number in array [1-100]
+ 12) find length of string/integer
+ 13) How many time maximum number comes.
+ 14) Convert '09:30AM' to 24 hours format
+ 15) print out [1-100], multiple of 3 as fizz, multiple of 5 as buzz 3x5 as fizzbuzz
+ 16) greatest common diviser of 4 , 6 // 2
+ 17) n kids, y toys, i start position... who gets the last TOY
+
+*/
+const x = {
+  a: 1,
+  b: 2,
+};
+
+const completeObject = Object.entries(x);
+const objVal = Object.values(x);
+const objKeys = Object.keys(x);
+
+console.log('1) ' + completeObject + ' ' + objVal + ' ' + objKeys + '-----------------------------------');
+
+const x1 = 'man';
+
+const reverseString = x1.split('').reverse().join('');
+
+console.log(`2) ${reverseString}
+-----------------------------------`);
+
+const obj = {
+  a: {
+    b: {
+      c: 1,
+    },
+  },
+};
+
+const cloneObj = JSON.parse(JSON.stringify(obj));
+const cloneObj_2 = Object.assign({}, a);
+
+console.log(`3) ${cloneObj.a.b.c}
+-----------------------------------`);
+
+const arr1 = [1, 2, 5, 4, 5];
+const arr2 = [4, 5, 6, 7, 8, 9];
+
+const arr3 = [...arr1, ...arr2];
+const uniqueArray = [...new Set(arr3)];
+console.log(uniqueArray);
+console.log(arr3);
+console.log(`${arr3.sort().reverse()}
+-----------------------------------`);
+
+const obj1 = {
+  x2: 1,
+  getX() {
+    // const that = this;  2nd Solution
+    const innner = () => {
+      // function(){
+      console.log(this.x2);
+    };
+    innner(); // innner.bind(this)(); 3rd Sol
+  },
+};
+obj1.getX();
+console.log('-----------------------------------');
+
+function add(number) {
+  return number + 2;
+}
+console.log(add(2));
+
+const addd = (num) => num + 2;
+console.log(addd(2));
+console.log('-----------------------------------');
+
+/* let array2 = [1,2,3,4,5];
+// add
+
+const result =  array2.reduce(  ) 
+ */
+
+const arr = [1, 2, 3, 4];
+
+const plus5 = arr.map((val, i, arr) => {
+  return val + 5;
+});
+console.log(plus5);
+console.log('-----------------------------------');
+
+let arr5 = arr.map((val, i, arr) => {
+  return {
+    value: val,
+    index: i,
+  };
+});
+console.log(arr5);
+console.log('-----------------------------------');
+
+let evenArr = arr.map((v, i, a) => (v % 2 === 0 ? v * 2 : v));
+console.log(evenArr);
+console.log('-----------------------------------');
+
+let data = [
+  {
+    country: 'China',
+    population: 1409517397,
+  },
+  {
+    country: 'India',
+    population: 1339180127,
+  },
+  {
+    country: 'USA',
+    population: 324459463,
+  },
+  {
+    country: 'Indonesia',
+    population: 263991379,
+  },
+];
+
+const data50 = data.filter((v) => v.population > 524459463);
+console.log(data50);
+console.log('-----------------------------------');
+
+let data1 = [
+  {
+    name: 'Butters',
+    age: 3,
+    type: 'dog',
+  },
+  {
+    name: 'Lizzy',
+    age: 6,
+    type: 'dog',
+  },
+  {
+    name: 'Red',
+    age: 1,
+    type: 'cat',
+  },
+  {
+    name: 'Joey',
+    age: 3,
+    type: 'dog',
+  },
+];
+
+console.log(data1);
+console.log('-----------------------------------');
+
+let sumData = data1.filter((a) => a.type === 'dog').map((b) => b.age * 7);
+
+console.log(sumData);
+console.log('-----------------------------------');
+
+let ages1 = data1
+  .filter((a) => a.type === 'dog')
+  .map((a) => a.age * 7)
+  .reduce((a, b) => a + b);
+
+console.log(ages1);
+
+console.log('-----------------------------------');
+
+let isDog = (a) => a.type === 'dog';
+let dogYears = (a) => a.age * 7;
+let sumOfAges = (a, b) => a + b;
+
+let ages2 = data1.filter(isDog).map(dogYears).reduce(sumOfAges);
+
+console.log(ages2);
+
+console.log('-----------------------------------');
+
+let adding = (x) => (y) => x + y;
+
+console.log(adding(2)(3));
+
+console.log('-----------------------------------');
+
+let array3 = [1, 2, 3, 4, 5];
+console.log(Math.min(...array3), Math.max(...array3));
+
+console.log('-----------------------------------');
+
+let array4 = [4, 5, 2, 1, 6, 7, 8, 9, 10];
+
+let arraySort = array4.sort((a, b) => a - b);
+console.log(arraySort);
+
+for (let i = 1; i < arraySort.length; i++) {
+  if (arraySort[i - 1] !== i) {
+    console.log(i);
+    break;
+  }
+}
+console.log('-----------------------------------');
+
+let xx = 1234;
+
+console.log(`12) ${xx.toString().length}`);
+
+console.log('-----------------------------------');
+
+let array5 = [3, 2, 2, 2, 4, 2, 4];
+
+let count = 0;
+
+let maxNumber = Math.max(...array5);
+
+array5.forEach((x) => {
+  if (x === maxNumber) {
+    count = count + 1;
+  }
+});
+
+console.log('13) ' + count);
+
+console.log('-----------------------------------');
+
+let time = '9:30PM';
+
+let splitTime = time.split(':');
+
+let hours = parseInt(splitTime[0]);
+let minutes = splitTime[1].includes('AM') ? splitTime[1].replace('AM', '') : splitTime[1].replace('PM', '');
+
+splitTime[1].includes('AM')
+  ? console.log(`Time is 0${hours}:${minutes}`)
+  : console.log(`14) Time is ${hours + 12}${minutes}
+-----------------------------------`);
+
+const toHex = (num) => num.toString(16).toUpperCase();
+
+console.log(`${toHex(2)}
+-----------------------------------`);
+
+function fizzBuzz() {
+  for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0) {
+      console.log('fizz');
+    }
+    if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz();
+
+console.log('-----------------------------------');
+
+const greatestDivisor = (a, b) => {
+  console.log(a, b);
+
+  let divisors = [];
+
+  let min = Math.min(a, b);
+  let max = a < min ? a : b;
+
+  console.log(`min is: ${min}
+max is: ${max}`);
+
+  for (let i = 1; i <= max; i++) {
+    if (min % i === 0 && max % i === 0) {
+      divisors.push(i);
+    }
+  }
+  console.log(divisors);
+  console.log('17) ' + Math.max(...divisors));
+};
+
+greatestDivisor(12, 24);
+console.log('-----------------------------------');
+
+/* // 5 , 3 , 2...
+
+let kids = [1,2,3,4,5];
+let toys = ['a','a','a','a','a','a'];
+let position = 0;
+
+let lastKid = (k, y, index) => {
+    let kidsToy = [];
+       for (let j = 0; j < y.length; j++) {
+            kidsToy.push(y[j])
+       }
+    console.log(kidsToy);
+}
+
+lastKid(kids, toys, position); */
+
+average = (input) => input.reduce((a, b) => a + b) / input.length;
+
+const input = [3, 5, 9, 15];
+let total = 0;
+input.forEach((element) => {
+  total += element;
+});
+// console.log(total/input.length);
+console.log(`18) ${average(input)} `);
+
+// -----------------------------
+function getActiveItems(obj) {
+  const activeItems = obj.items.filter((item) => item.active === true);
+  const effectiveDate = obj.effectiveDate;
+
+  activeItems.forEach((element) => {
+    element.effectiveDate = effectiveDate;
+  });
+  return activeItems;
+}
+
+const dataaa = {
+  items: [
+    {
+      name: 'Item One',
+      active: true,
+    },
+    {
+      name: 'Item Two',
+      active: false,
+    },
+    {
+      name: 'Item Three',
+      active: true,
+    },
+  ],
+  effectiveDate: '6/21/2017',
+};
+
+console.log(getActiveItems(dataaa));
